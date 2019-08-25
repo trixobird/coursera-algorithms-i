a2e1198174d65cf9a7f024d36901ad1f99cccd9a
trixobird <george.trichopoulos@gmail.com>
2019-08-25 22:39:49 +0300

    Percolation Implementation

--- percolation/src/main/java/Percolation.java
+++ percolation/src/main/java/Percolation.java
@@ -1,2 +1,102 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+    private final int size;
+    private final WeightedQuickUnionUF unionFind;
+    private final boolean[] open;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+        size = n;
+        open = new boolean[(int) (Math.pow(size, 2) + 2)];
+        open[0] = true;
+        open[open.length - 1] = true;
+        for (int i = 1; i < open.length - 1; i++) {
+            open[i] = false;
+        }
+
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF((int) (Math.pow(n, 2) + 2));
+        for (int i = 1; i <= n; i++) {
+            uf.union(0, i);
+        }
+        for (int i = (int) (Math.pow(n, 2) - n + 1); i <= Math.pow(n, 2); i++) {
+            uf.union((int) (Math.pow(n, 2) + 1), i);
+        }
+        unionFind = uf;
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+        validateSquare(row, col);
+        open[to1D(row, col)] = true;
+
+        if (col > 1 && open[to1D(row, col - 1)]) {
+            unionFind.union(to1D(row, col), to1D(row, col - 1));
+        }
+        if (col < size && open[to1D(row, col + 1)]) {
+            unionFind.union(to1D(row, col), to1D(row, col + 1));
+        }
+        if (row > 1 && open[to1D(row - 1, col)]) {
+            unionFind.union(to1D(row, col), to1D(row - 1, col));
+        }
+        if (row < 1 && open[to1D(row + 1, col)]) {
+            unionFind.union(to1D(row, col), to1D(row + 1, col));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+        validateSquare(row, col);
+        return open[to1D(row, col)];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+        validateSquare(row, col);
+        return unionFind.connected(0, to1D(row, col));
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (boolean b : open) {
+            if (b) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return unionFind.connected(0, (int) Math.pow(size, 2) + 1);
+    }
+
+    // UncommentedEmptyMethodBody
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    private void validateSquare(int row, int col) {
+        validateSide(row);
+        validateSide(col);
+    }
+
+    private void validateSide(int side) {
+        if (side > size
+                || side <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int to1D(int row, int col) {
+        return size * (col - 1) + row;
+    }
 }
--- percolation/src/main/java/PercolationStats.java
+++ percolation/src/main/java/PercolationStats.java
@@ -1,2 +1,59 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private final double[] mean;
+    private final int trials;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        this.trials = trials;
+        mean = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int nextSiteToOpen = (int) StdRandom.uniform(1,  Math.pow(n, 2));
+                while (percolation.isOpen(getRow(n, nextSiteToOpen), getCol(n, nextSiteToOpen))) {
+                    nextSiteToOpen = (int) StdRandom.uniform(1, Math.pow(n, 2));
+                }
+                percolation.open(getRow(n, nextSiteToOpen), getCol(n, nextSiteToOpen));
+            }
+            mean[i] = percolation.numberOfOpenSites();
+        }
+    }
+
+    private int getCol(int n, int nextSiteToOpen) {
+        return ((nextSiteToOpen - 1) % n) + 1;
+    }
+
+    private int getRow(int n, int nextSiteToOpen) {
+        return ((nextSiteToOpen - 1) / n) + 1;
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(mean);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(mean);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / StrictMath.sqrt(trials);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / StrictMath.sqrt(trials);
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
+    }
 }
